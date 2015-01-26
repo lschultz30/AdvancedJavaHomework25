5706853eb1ec3b5b736a5b3bc1dc0a23ca6e88be
Lane Schultz <Lane Schultz@10.30.19.223>
2015-01-26 15:44:40 -0600

    First Commit

---
+++ src/advancedjavahomework2/Dwarf.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package advancedjavahomework2;
+
+/**
+ *
+ * @author Lane Schultz
+ */
+public class Dwarf {
+     private String skinColor;
+     private String height;
+     private Boolean humanoid;
+}
